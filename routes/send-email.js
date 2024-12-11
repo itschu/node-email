3ef71960e2703605557fa98d81b42007757cf1fc
itschu <palmerbideconcepts@gmail.com>
2024-12-11 11:04:54 +0100

    added senders name

--- routes/send-email.js
+++ routes/send-email.js
@@ -22,7 +22,7 @@ router.post('/', async (req, res) => {
 
 	try {
 		await transporter.sendMail({
-			from: sender,
+			from: `${sender} <${user}>`,
 			to: to,
 			replyTo: replyTo,
 			subject: subject,
